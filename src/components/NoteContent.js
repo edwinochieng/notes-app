b7c8d0e910d4d49285e59285e9f0c7b4ff966128
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-08-05 04:08:08 +0300

    change note card height to auto

--- src/components/NoteContent.js
+++ src/components/NoteContent.js
@@ -6,7 +6,7 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 function NoteContent({note}) {
    const{deleteNote} = useContext(GlobalContext);
   return (
-    <div className='relative border border-solid mx-2 my-1 px-3 py-2 h-fit w-82 border border-solid border-2 border-zinc-100 rounded-lg bg-neutral-50'>
+    <div className='relative border border-solid mx-2 my-1 px-3 py-2 h-auto w-82 border border-solid border-2 border-zinc-100 rounded-lg bg-neutral-50'>
     <div>
     <h1 className='font-quicksand pb-1'>{note.title}</h1>
         <p className='font-urbanist'>{note.content}</p>
