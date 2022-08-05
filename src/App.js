ecc6602b83bec3538a57502184db2baffcc9c7dd
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-08-05 03:18:26 +0300

    add context

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React from "react";
 import Header from "./components/Header";
 import Notes from "./components/Notes"
 import CreateNote from "./components/CreateNote";
-
+import {GlobalProvider} from "./context/GlobalContext"
 
 function App() {
   return (
     <div className="h-full bg-zinc-300">
 
+    <GlobalProvider>
     <Header/>
     <div className="flex flex-col items-center">
     <CreateNote/>
     <Notes/>
     </div>
    
+    
+    </GlobalProvider>
+
     
     
     </div>
