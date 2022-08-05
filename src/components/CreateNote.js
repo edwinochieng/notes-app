7d7a571163b2cdb57082657389e5a1937e9f2261
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-08-05 04:05:16 +0300

    add onsubmit event handler

--- src/components/CreateNote.js
+++ src/components/CreateNote.js
@@ -1,12 +1,29 @@
-import React,{useState} from "react";
+import React,{useContext, useState} from "react";
+import {GlobalContext} from '../context/GlobalContext'
 
 function CreateNote() {
     const[title,setTitle] = useState("");
     const [content, setContent] = useState("");
+    const {addNote} = useContext(GlobalContext);
+    
+    function handleSubmit(e){
+      e.preventDefault();
+
+      const newNote = {
+          id: Math.floor(Math.random() * 10000000),
+          title,
+          content
+
+      }
+      addNote(newNote);
+    }
+    
+
+
 
   return (
     <div className="relative bg-neutral-50 border border-solid border-zinc-100 border-2 rounded-xl mt-4 w-128 h-50 pt-4">
-      <form className="flex flex-col items-start pl-7">
+      <form  onSubmit = {handleSubmit} className="flex flex-col items-start pl-7">
         <input
           onChange={(e) => setTitle(e.target.value)}
           className="mb-2 px-2 w-116 border border-solid rounded-lg focus: outline-none"
@@ -23,7 +40,7 @@ function CreateNote() {
           cols="70"
           placeholder="Enter Note..."
         />
-        <button className="absolute bottom-1 right-8 border border-solid rounded-xl w-16 h-6 bg-gray-700 text-white text-sm font-quicksand">
+        <button type="submit" className="absolute bottom-1 right-8 border border-solid rounded-xl w-16 h-6 bg-gray-700 text-white text-sm font-quicksand">
           Add
         </button>
       </form>
