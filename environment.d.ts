95095c2fed1255c381e538d6727cd6c76a7ed9ef
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-24 23:25:26 +0300

    environment variables types

---
+++ environment.d.ts
@@ -0,0 +1,11 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production";
+    GITHUB_ID: string;
+    GITHUB_SECRET: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    NEXTAUTH_URL: string;
+    NEXTAUTH_SECRET: string;
+  }
+}
