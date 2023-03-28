2b8abb8ed8704eab1dbfcf847309b1cabb880251
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-28 21:39:14 +0300

    add react-hot-toaster and react-query providers

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,7 +2,17 @@
 
 import React from "react";
 import { SessionProvider } from "next-auth/react";
+import { Toaster } from "react-hot-toast";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  const queryClient = new QueryClient();
+  return (
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster position='top-center' />
+        {children}
+      </QueryClientProvider>
+    </SessionProvider>
+  );
 }
