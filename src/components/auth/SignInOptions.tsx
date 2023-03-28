3e5ac6d8b89b7396d79ead565ef08533201364ef
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-28 19:35:11 +0300

    redirect to homepage after authentication

--- src/components/auth/SignInOptions.tsx
+++ src/components/auth/SignInOptions.tsx
@@ -1,23 +1,36 @@
 "use client";
 
 import React from "react";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { BsGithub } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
+import { useRouter } from "next/navigation";
 
 export default function SignInOptions() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const handleSignIn = (provider: string) => {
+    signIn(provider, { callbackUrl: "/" });
+  };
+
+  if (session) {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+    return <div className='text-gray-800'>You are already signed in</div>;
+  }
   return (
     <div className='max-w-[250px] mx-auto flex flex-col items-center space-y-2 text-sm'>
       <button
         className='w-full bg-white hover:bg-gray-200 text-black font-semibold py-2 px-4 rounded-md flex items-center justify-center'
-        onClick={() => signIn("google")}
+        onClick={() => handleSignIn("google")}
       >
         <FcGoogle className='inline-block mr-2' size={18} />
         Sign in with Google
       </button>
       <button
         className='w-full bg-gray-800 hover:bg-gray-700 text-white font-medium py-2 px-4 rounded-md flex items-center justify-center'
-        onClick={() => signIn("github")}
+        onClick={() => handleSignIn("github")}
       >
         <BsGithub className='inline-block mr-2' size={18} />
         Sign in with GitHub
