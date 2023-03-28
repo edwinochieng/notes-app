af8990690b1d3e634f21b512e519b32c52e2853c
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-28 20:50:01 +0300

    add user image

--- src/components/navbar/Nav.tsx
+++ src/components/navbar/Nav.tsx
@@ -4,14 +4,28 @@ import React from "react";
 import { useSession } from "next-auth/react";
 import SignOut from "../auth/buttons/SignOut";
 import SignIn from "../auth/buttons/SignIn";
+import Image from "next/image";
 
 export default function Nav() {
   const { data: session } = useSession();
 
   return (
-    <div className='fixed top-0 left-0 w-full flex justify-between'>
+    <div className='fixed top-0 left-0 w-full flex justify-between py-1'>
       <div>Home</div>
-      <div>{session ? <SignOut /> : <SignIn />}</div>
+      <div className='flex items-center'>
+        <div>{session ? <SignOut /> : <SignIn />}</div>
+        {session && (
+          <div className='ml-1'>
+            <Image
+              src={session?.user.image!}
+              alt='profile'
+              height={35}
+              width={35}
+              className='rounded-full'
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
