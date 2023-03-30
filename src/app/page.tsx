5a49b6b7dd8f635725a4ba938de0a1e1144d093c
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-30 22:27:25 +0300

    fetch notes from db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import Nav from "@/components/navbar/Nav";
+import AddNote from "@/components/notes/AddNote";
+import Note from "@/components/notes/Note";
 import React from "react";
 
+const fetchNotes = async () => {
+  const res = await fetch(`${process.env.BASE_URL}/api/notes/getNotes`);
+
+  if (!res.ok) {
+    throw new Error("Data not fetched");
+  }
+
+  return res.json();
+};
 export default async function Home() {
+  const notes = await fetchNotes();
+  console.log(notes);
+
   return (
     <div>
       <Nav />
+      <div className='mt-16'>
+        <AddNote />
+      </div>
+      <div></div>
     </div>
   );
 }
