ab6b8c38e76c92a8310c110761794a5b87066ec0
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-31 22:37:32 +0300

    delete note api call

--- src/components/notes/DeleteNote.tsx
+++ src/components/notes/DeleteNote.tsx
@@ -7,15 +7,40 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
+import { toast } from "react-hot-toast";
 
-export default function DeleteNote() {
+export default function DeleteNote({ id }: { id: string }) {
   const [open, setOpen] = React.useState(false);
+  const queryClient = useQueryClient();
 
   const handleClickOpen = () => {
     setOpen(true);
   };
 
   const handleClose = () => {
+    setOpen(false);
+  };
+
+  const { mutate } = useMutation(
+    async (id: string) =>
+      await axios.delete(`/api/notes/${id}/deleteNote`, { data: id }),
+    {
+      onError: (error) => {
+        if (error instanceof AxiosError) {
+          toast.error("Error deleting note");
+        }
+      },
+      onSuccess: (data) => {
+        queryClient.invalidateQueries(["notes"]);
+        toast.success("Note has been deleted");
+      },
+    }
+  );
+
+  const handleDelete = () => {
+    mutate(id);
     setOpen(false);
   };
 
@@ -30,9 +55,7 @@ export default function DeleteNote() {
         aria-labelledby='alert-dialog-title'
         aria-describedby='alert-dialog-description'
       >
-        <DialogTitle id='alert-dialog-title'>
-          {"Use Google's location service?"}
-        </DialogTitle>
+        <DialogTitle id='alert-dialog-title'>{"Delete Note"}</DialogTitle>
         <DialogContent>
           <DialogContentText id='alert-dialog-description'>
             Are you sure you want to delete this note?
@@ -40,7 +63,7 @@ export default function DeleteNote() {
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleClose} autoFocus>
+          <Button onClick={handleDelete} autoFocus>
             Delete
           </Button>
         </DialogActions>
