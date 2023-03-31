03ce9b9d864c5bf46d50a3d693ce12a01565f817
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-31 22:40:22 +0300

    edit and delete note buttons

--- src/components/notes/Note.tsx
+++ src/components/notes/Note.tsx
@@ -1,7 +1,10 @@
 "use client";
 import React from "react";
+import EditNote from "./EditNote";
+import DeleteNote from "./DeleteNote";
 
 type Note = {
+  id: string;
   title: string;
   content: string;
 };
@@ -15,6 +18,14 @@ export default function Note({ note }: Props) {
     <div className='bg-white border border-gray-400 rounded-md p-4'>
       <h2 className='font-medium text-lg'>{note.title}</h2>
       <p className='text-gray-600 mt-2'>{note.content}</p>
+      <div className='flex'>
+        <div>
+          <EditNote id={note.id} />
+        </div>
+        <div>
+          <DeleteNote id={note.id} />
+        </div>
+      </div>
     </div>
   );
 }
