4e4f0fe1e7a15d3a792056d4b0282887207821ad
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-03-31 16:55:32 +0300

    fetch all notes from db using react-query

---
+++ src/components/notes/NoteList.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import Note from "./Note";
+
+const fetchNotes = async () => {
+  const res = await axios.get("/api/notes/getNotes");
+
+  return res.data;
+};
+
+export default function NoteList() {
+  const { data, isLoading, isError, error } = useQuery({
+    queryFn: fetchNotes,
+    queryKey: ["notes"],
+  });
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    return <p>An error occurred: {(error as Error).message}</p>;
+  }
+
+  return (
+    <div>
+      {data?.map((note: any) => (
+        <Note key={data?.id} note={note} />
+      ))}
+    </div>
+  );
+}
