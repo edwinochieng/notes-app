cb3ba1e6edeb645fb138b9843f11bf93bba843a6
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-04-06 22:31:55 +0300

    fit content in notes component

--- src/components/notes/Note.tsx
+++ src/components/notes/Note.tsx
@@ -15,9 +15,11 @@ interface Props {
 
 export default function Note({ note }: Props) {
   return (
-    <div className='bg-white shadow-lg rounded-md p-2  w-[350px]'>
+    <div className=' bg-white shadow-lg rounded-md p-2 w-full'>
       <h2 className='font-medium text-[16px]'>{note.title}</h2>
-      <p className='text-[15px] text-gray-600 mt-[1px]'>{note.content}</p>
+      <p className='text-[15px] text-gray-600 mt-[1px] break-words'>
+        {note.content}
+      </p>
       <div className='mt-2 flex items-center justify-end'>
         <div className='mr-1'>
           <EditNote note={note} />
