87553df98869b0ad4e8d578baa3c597eb9a45891
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-04-07 19:46:17 +0300

    form validation with zod

--- src/components/notes/AddNote.tsx
+++ src/components/notes/AddNote.tsx
@@ -7,14 +7,26 @@ import axios, { AxiosError } from "axios";
 import toast from "react-hot-toast";
 
 const formSchema = z.object({
-  title: z.string(),
-  content: z.string().min(1).max(400),
+  title: z
+    .string()
+    .min(1, { message: "Title is required" })
+    .max(60, { message: "Title should not exceed 60 characters" }),
+  content: z
+    .string()
+    .min(1, { message: "Content is required" })
+    .max(350, { message: "Content should not exceed 350 characters" }),
 });
-
 type Note = z.infer<typeof formSchema>;
+
+interface NoteFormErrors {
+  title?: string;
+  content?: string;
+}
 
 export default function AddNote() {
   const [note, setNote] = useState<Note>({ title: "", content: "" });
+  const [errors, setErrors] = useState<NoteFormErrors>({});
+
   const queryClient = useQueryClient();
 
   const { mutate } = useMutation(
@@ -33,9 +45,17 @@ export default function AddNote() {
     }
   );
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    mutate(note);
+
+    try {
+      await formSchema.parse(note);
+      mutate(note);
+      setErrors({});
+    } catch (error: any) {
+      const errorMessage = error.errors[0]?.message;
+      setErrors({ [error.errors[0]?.path[0]]: errorMessage });
+    }
   };
 
   return (
@@ -50,8 +70,11 @@ export default function AddNote() {
         placeholder='Title'
         value={note.title}
         onChange={(e) => setNote({ ...note, title: e.target.value })}
-        className=' rounded-md p-2 border border-gray-100 outline-none '
+        className=' text-sm rounded-md py-1 px-2 border border-gray-100 outline-none '
       />
+      {errors.title && (
+        <span className='text-red-500 text-xs'>{errors.title}</span>
+      )}
 
       <textarea
         name='content'
@@ -59,11 +82,15 @@ export default function AddNote() {
         placeholder='Take a note...'
         value={note.content}
         onChange={(e) => setNote({ ...note, content: e.target.value })}
-        className=' rounded-md p-2 h-auto border border-gray-100 outline-none'
+        className=' text-sm rounded-md py-1 px-2 h-24 border border-gray-100 outline-none'
       ></textarea>
-
+      {errors.content && (
+        <span className='text-red-500 text-xs'>{errors.content}</span>
+      )}
       <div className='flex justify-between'>
-        <div>{note.content.length}/400</div>
+        <div className='text-sm font-semibold text-gray-700'>
+          {note.content.length}/350
+        </div>
         <button
           type='submit'
           className=' bg-gray-700 text-white py-1 px-5 rounded-md hover:bg-gray-800'
