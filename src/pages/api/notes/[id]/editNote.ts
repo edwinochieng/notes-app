c20de0649f9628ec976c090e1ced3934cbf27d04
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-04-08 21:32:06 +0300

    validate updated note

--- src/pages/api/notes/[id]/editNote.ts
+++ src/pages/api/notes/[id]/editNote.ts
@@ -1,5 +1,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../../../prisma/client";
+import { z } from "zod";
+
+const formSchema = z.object({
+  title: z
+    .string()
+    .min(1, { message: "Title is required" })
+    .max(60, { message: "Title should not exceed 60 characters" }),
+  content: z
+    .string()
+    .min(1, { message: "Content is required" })
+    .max(350, { message: "Content should not exceed 350 characters" }),
+});
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,7 +22,7 @@ export default async function handler(
   }
   const { data } = req.body;
 
-  console.log(data);
+  await formSchema.parse(data);
 
   try {
     const note = await prisma.note.update({
@@ -22,7 +34,11 @@ export default async function handler(
     });
 
     res.status(200).json({ message: "Edited Successfully" });
-  } catch (err) {
-    res.status(500).send({ message: "Server error" });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      res.status(400).json({ message: error.message });
+    } else {
+      res.status(500).json({ message: "Server error" });
+    }
   }
 }
