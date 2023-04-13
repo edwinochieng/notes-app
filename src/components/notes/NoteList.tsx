980c73141a5f2808ccd9ab5c9de1d5a5bacd8d88
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-04-13 21:06:03 +0300

    fix key error

--- src/components/notes/NoteList.tsx
+++ src/components/notes/NoteList.tsx
@@ -29,7 +29,7 @@ export default function NoteList() {
   return (
     <div className='mb-8 grid place-items-center items-start grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3'>
       {data?.map((note: any) => (
-        <Note key={data?.id} note={note} />
+        <Note key={note.id} note={note} />
       ))}
     </div>
   );
